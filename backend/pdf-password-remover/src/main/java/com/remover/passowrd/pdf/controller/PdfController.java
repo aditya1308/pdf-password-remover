0f0a58728c9578e2d0e40bb3260e761637fd1e64
Aditya Prasad <asmarty2016@gmail.com>
2026-02-11 23:24:03 +0530

    Debug

--- backend/pdf-password-remover/src/main/java/com/remover/passowrd/pdf/controller/PdfController.java
+++ backend/pdf-password-remover/src/main/java/com/remover/passowrd/pdf/controller/PdfController.java
@@ -2,6 +2,8 @@ package com.remover.passowrd.pdf.controller;
 
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +14,23 @@ import java.io.ByteArrayOutputStream;
 
 @RestController
 @RequestMapping("/api/pdf")
-@CrossOrigin(origins = "*") // change later to your frontend domain
+@CrossOrigin(origins = "*")
 public class PdfController {
+
+    private static final Logger log = LoggerFactory.getLogger(PdfController.class);
 
     @PostMapping(value = "/unlock", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<byte[]> unlockPdf(@RequestPart("file") MultipartFile file, @RequestPart("password") String password) {
 
-        try (PDDocument document =
-                     Loader.loadPDF(file.getBytes(), password)) {
+        log.info("Password : {}", password);
+        try (PDDocument document = Loader.loadPDF(file.getBytes(), password)) {
 
             document.setAllSecurityToBeRemoved(true);
 
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             document.save(out);
 
+            log.info("PDF unlocked successfully");
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION,
                             "attachment; filename=unlocked.pdf")
